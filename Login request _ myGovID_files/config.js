0ee375b39bf05cd0007e1385f2d361028211ad98
Josh Ryan <joshua.ryan@casa.gov.au>
2022-03-17 11:42:27 +1100

    initial commit

---
+++ Login request _ myGovID_files/config.js
@@ -0,0 +1,17 @@
+﻿window.clientConfig =
+{
+    apiServiceBase: 'https://mygovid.gov.au',
+    helpUrl: 'http://www.mygovid.gov.au',
+    myGovUrl: 'http://my.gov.au',
+    appStoreUrl: 'https://apps.apple.com/au/app/mygovid/id1397699449',
+    googlePlayUrl: 'https://play.google.com/store/apps/details?id=au.gov.ato.mygovid.droid',
+    windowsUrl: '',        
+    pollingTimeout: 65,
+    initialPollingIntervals: '8 2 1',
+    fixedPollingIntervals: 3,
+    serverNotificationTransports: 'webSockets serverSentEvents',
+    stub: false,
+    sendAuthRequestRetries: 2,
+    appVersion: '2.1.37.2',
+    debugMode: true
+}
